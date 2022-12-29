f4291a8136a7d602138cc3fba0a1f88faec5d2f2
vitalik <vitalik-s-hedgehog@yandex.ru>
2022-12-29 20:19:35 +0300

    первое задание

---
+++ dz 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите нужное количество чисел -> ");
+string[] stringNum = Console.ReadLine()!.Split(' ');
+int count = 0;
+
+for (int i = 0; i < stringNum.Length-1; i++)
+{
+    string Q = Convert.ToString(stringNum[i]);
+    int W = Convert.ToInt32(Q);
+    if (W > 0) count++;
+}
+Console.Write(count);
